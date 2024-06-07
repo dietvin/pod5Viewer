d7d83321acf4d7f84a3c701cdf6666a9e4dfaec2
dietvin <dietricv@uni-mainz.de>
2024-06-07 11:09:48 +0200

    Added files for pip install

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+exec(open("pod5Viewer/__version__.py").read())
+
+setup(name="pod5Viewer",
+      version=__version__, # type: ignore (imported from __version__.py above)
+      description="GUI for inspecting ONT pod5 files",
+      author="Vincent Dietrich",
+      author_email="dietricv@uni-mainz.de",
+      url="https://github.com/dietvin/pod5Viewer",
+      license="LICENSE",
+      packages=["pod5Viewer"],
+      entry_points={"console_scripts": ["pod5Viewer = pod5Viewer.__main__:main",],},
+      python_requires=">3.9",
+      install_requires=["pod5==0.3.10",
+                        "pyside6==6.7.1",
+                        "setuptools==70.0.0"],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Scientific/Engineering :: Bio-Informatics",
+                   "Operating System :: OS Independent"])
