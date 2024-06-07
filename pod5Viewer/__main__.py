7ed895c15d02c307fc7f0d3d62b688a5a9025850
dietvin <dietricv@uni-mainz.de>
2024-06-07 11:09:30 +0200

    Set up package properly

---
+++ pod5Viewer/__main__.py
@@ -0,0 +1,20 @@
+try:
+    from .pod5Viewer import Pod5Viewer
+except:
+    from pod5Viewer import Pod5Viewer
+    
+from PySide6.QtWidgets import QApplication
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+
+    file_paths = sys.argv[1:] if len(sys.argv) > 1 else None
+
+    window = Pod5Viewer(file_paths)
+    window.show()
+    sys.exit(app.exec())
+    
+
+if __name__=="__main__":
+    main()
